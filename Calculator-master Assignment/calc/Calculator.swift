c64ede1f2950fc64a9279a6a61340fd31d24c292
Dylan-KA <111679436+Dylan-KA@users.noreply.github.com>
2024-03-16 17:03:29 +1100

    Stack Method

--- Calculator-master Assignment/calc/Calculator.swift
+++ Calculator-master Assignment/calc/Calculator.swift
@@ -10,10 +10,12 @@ import Foundation
 
 class Calculator {
     
-    var numbers :[Int] = []
-    var operators :[String] = []
-    let stringChecker = StringChecker()
+    var inputStack :[String] = []
+    var num1 :Int = 0
+    var num2 :Int = 0
+    var oper :String = ""
     let errorHandling = ErrorHandling()
+    
     
     func add(no1: Int, no2: Int) -> Int {
         return no1 + no2
@@ -42,44 +44,49 @@ class Calculator {
     func calculate(args: [String]) -> String {
         
         //Dummy args
-        let dummyArgs = ["1", "+", "2", "+", "3", "+", "4"]
+        //let dummyArgs = ["1", "+", "2", "+", "3", "+", "4"]
         
-        errorHandling.checkForAllErrorTypes(arguments: dummyArgs)
+        errorHandling.checkForAllErrorTypes(arguments: args)
         
-        //Iterate through args to find which are numbers and which are operators
-        for str in dummyArgs {
-            if (stringChecker.isNumber(str: str)) {
-                if let num = Int(str) {
-                    numbers.append(num)
-                }
-            }
-            if (stringChecker.isOperator(str: str)) {
-                operators.append(str)
-            }
+        for str in args {
+            inputStack.append(str)
         }
         
-        //Return early if only a single integer is given (useful for recursive calls)
-        if (operators.count == 0) {
-            return String(numbers[0])
+        //Return early if only a single integer is given
+        if (inputStack.count == 1) {
+            return String(inputStack[0])
         }
-        
-        var result :Int = 0
-        
-        //TO DO: Precedence Function to sort the order of operations
                 
-        if (operators.count == 1) {
+        //TO DO: Precedence Function to sort the order of operations
+        
+        while inputStack.count > 1 {
+            var result :Int = 0
             
-            switch operators[0] {
+            if let num1Optional = Int(inputStack[0]) {
+                num1 = num1Optional
+                }
+            oper = inputStack[1]
+            if let num2Optional = Int(inputStack[2]) {
+                num2 = num2Optional
+                }
+            inputStack.removeFirst(3)
+            
+            switch oper {
             case "+":
-                result = add(no1: numbers[0], no2: numbers[1])
+                result = add(no1: num1, no2: num2)
+                inputStack.insert(String(result), at: 0)
             case "-":
-                result = subtract(no1: numbers[0], no2: numbers[1])
+                result = subtract(no1: num1, no2: num2)
+                inputStack.insert(String(result), at: 0)
             case "/":
-                result = divide(no1: numbers[0], no2: numbers[1])
+                result = divide(no1: num1, no2: num2)
+                inputStack.insert(String(result), at: 0)
             case "x":
-                result = multiply(no1: numbers[0], no2: numbers[1])
+                result = multiply(no1: num1, no2: num2)
+                inputStack.insert(String(result), at: 0)
             case "%":
-                result = remainder(no1: numbers[0], no2: numbers[1])
+                result = remainder(no1: num1, no2: num2)
+                inputStack.insert(String(result), at: 0)
             default:
                 // This error should already be caught by
                 // errorHandling.invalidOperator
@@ -88,7 +95,7 @@ class Calculator {
                 exit(EXIT_FAILURE)
             }
         }
-        
-        return("\(result)")
+
+        return("\(inputStack[0])")
     }
 }
